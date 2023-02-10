e0f75280672cc3d72add9d5e407ed169b4822209
Ashmit JaiSarita Gupta <ashmitgupta.official@gmail.com>
2023-02-10 13:41:43 +0530

    Added hover border color to input boxex in login component

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -16,20 +16,20 @@ export default function Login() {
             <p className='text-base font-semibold mb-4 text-[#727986]'>Enter your details to log into your Brainout Account:</p>
             <div className='flex flex-col mb-4'>
                 <label className='text-sm font-semibold mb-2.5'>Phone Number or Email*</label>
-                <input className='border p-2 rounded-[10px] text-sm font-light h-[30px]' type="text" placeholder='natasha.malkova@outlook.com' />
+                <input className='border hover:border-[#0090FC] p-2 rounded-[10px] text-sm font-light h-[30px]' type="text" placeholder='natasha.malkova@outlook.com' />
             </div>
             <div className='flex flex-col mb-4 relative'>
                 <label className='text-sm font-semibold mb-2.5'>Password*</label>
-                <input className='border p-2 rounded-[10px] text-sm font-light h-[30px]' type={(showPassword === false) ? "password" : "text"} placeholder='*********' />
+                <input className='border hover:border-[#0090FC] p-2 rounded-[10px] text-sm font-light h-[30px]' type={(showPassword === false) ? "password" : "text"} placeholder='*********' />
                 <div className='absolute right-2 top-10 cursor-pointer'>
                 {
                   (showPassword === false) ?
                     <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleShowPassword}>
-                      <path d="M14.6667 1.33334C14.6667 1.33334 12 5.33334 8.00004 5.33334C4.00004 5.33334 1.33337 1.33334 1.33337 1.33334" stroke="#2A353D" stroke-linecap="round"/>
-                      <path d="M10 5L11 6.66667" stroke="#2A353D" stroke-linecap="round" strokeLinejoin="round"/>
-                      <path d="M13.3334 3.33334L14.6667 4.66667" stroke="#2A353D" stroke-linecap="round" strokeLinejoin="round"/>
-                      <path d="M1.33337 4.66667L2.66671 3.33334" stroke="#2A353D" stroke-linecap="round" strokeLinejoin="round"/>
-                      <path d="M6 5L5 6.66667" stroke="#2A353D" stroke-linecap="round" strokeLinejoin="round"/>
+                      <path d="M14.6667 1.33334C14.6667 1.33334 12 5.33334 8.00004 5.33334C4.00004 5.33334 1.33337 1.33334 1.33337 1.33334" stroke="#2A353D" strokeLinecap="round"/>
+                      <path d="M10 5L11 6.66667" stroke="#2A353D" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M13.3334 3.33334L14.6667 4.66667" stroke="#2A353D" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M1.33337 4.66667L2.66671 3.33334" stroke="#2A353D" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M6 5L5 6.66667" stroke="#2A353D" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   :
                   <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleShowPassword}>
@@ -41,7 +41,7 @@ export default function Login() {
             </div>
             <div className='flex items-center'>
                 <label htmlFor="remember-checkbox" className='cursor-pointer relative'>
-                    <input type="checkbox" id="remember-checkbox" className='appearance-none w-4 h-4 border-[1.5px] border-black rounded'/>
+                    <input type="checkbox" id="remember-checkbox" className='appearance-none w-4 h-4 border-[1.5px] border-black hover:border-[#0090FC] rounded'/>
                     <svg width="9" height="8" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg" className='absolute left-1 top-[6px] hidden'>
                         <path d="M1.05261 5.05263C1.05261 5.05263 2.39998 5.82107 3.07367 6.94736C3.07367 6.94736 5.09472 2.52631 7.78945 1.05263" stroke="#2A353D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg> 
