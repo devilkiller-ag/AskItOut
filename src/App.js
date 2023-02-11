c94f946ac77c1ad23fa99bee2478ef928c8e2b3b
Ashmit JaiSarita Gupta <ashmitgupta.official@gmail.com>
2023-02-11 14:32:48 +0530

    Completed all welcome pages and added routes to all pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-// import Welcome1 from './Components/Welcome1';
-import Home from './Components/Home';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Welcome1 from './Components/Welcome1';
+import Welcome2 from './Components/Welcome2';
+import Welcome3 from './Components/Welcome3';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route exact path='/' element={ <Home /> } />
           <Route exact path='/login' element={ <Login /> } />
           <Route exact path='/signup' element={ <Signup /> } />
+          <Route exact path='/welcome1' element={ <Welcome1 /> } />
+          <Route exact path='/welcome2' element={ <Welcome2 /> } />
+          <Route exact path='/welcome3' element={ <Welcome3 /> } />
         </Routes>
       </Router>   
     </>
